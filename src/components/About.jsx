c6e0851adb03a107d57e89f5750a360bd9eab0aa
Sanjula Deshan <95172391+SanjulaDeshan@users.noreply.github.com>
2023-09-20 19:17:26 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,21 +14,14 @@ export default function About() {
         </div>
 
         <p className="mt-20 text-xl text-justify">
-          Hello there 👋 I am an ambitious and dedicated IT undergraduate
-          student with a strong passion for technology. I am always looking for
-          new challenges and opportunities to learn and grow. I am confident
-          that my technical knowledge, creativity, and work ethic would make me
-          a valuable asset to any team. I am proficient in a variety of
-          technologies. I am also experienced in using various tools and
-          technologies. I am a team player who is eager to contribute and learn
-          from others. I am also a self-starter who is motivated to complete
-          tasks on time and to a high standard. I am passionate about using
-          technology to solve real-world problems. I am always looking for ways
-          to use my skills to make a positive impact on the world. Please
-          explore my portfolio to see my projects and my journey as an aspiring
-          IT professional. I am always looking for new challenges and
-          opportunities to learn and grow. I am confident that I have the skills
-          and drive to succeed in this field.
+          I am an IT undergraduate student with a passion for technology and a
+          strong work ethic. I am proficient in a variety of technologies and
+          have experience using various tools and technologies. I am a team
+          player and a self-starter who is motivated to complete tasks on time
+          and to a high standard. I am passionate about using technology to
+          solve real-world problems and I am always looking for new challenges
+          and opportunities to learn and grow. In other words, I am a highly
+          motivated and skilled IT student with a bright future ahead of me.
         </p>
       </div>
     </div>
